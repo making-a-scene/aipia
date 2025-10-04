33e46dbb4f3c73623a2f92103b2e0a33f7898b54
making-a-scene <4936joo@naver.com>
2025-10-04 16:15:23 +0900

    feat: MemberRepository 생성

---
+++ src/main/java/com/aipia/tesk/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.aipia.tesk.repository;
+
+import com.aipia.tesk.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, String> {
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
