e0b149dbbb1f23a440c5390a6086ee4943b3fac8
making-a-scene <4936joo@naver.com>
2025-10-04 15:12:58 +0900

    chore: 프로젝트 생성

---
+++ src/test/java/com/aipia/tesk/TeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aipia.tesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
