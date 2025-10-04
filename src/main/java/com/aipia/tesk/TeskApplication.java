13fb1d1a408ee905b9e7def96e76f6ff531a87f0
making-a-scene <4936joo@naver.com>
2025-10-04 14:58:30 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/aipia/tesk/TeskApplication.java
@@ -0,0 +1,13 @@
+package com.aipia.tesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeskApplication.class, args);
+	}
+
+}
